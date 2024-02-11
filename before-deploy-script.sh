d483d657ca5f73c8abff77959317c1cbecc0cb4d
Mohammad Mahmud Hasan <34007876+mahmud92542@users.noreply.github.com>
2024-02-11 22:37:53 +0600

    Update before-deploy-script.sh

--- before-deploy-script.sh
+++ before-deploy-script.sh
@@ -6,8 +6,6 @@ pm2 delete nextjs
 # Remove all files and folders inside /home/ubuntu/devops-assessment/
 rm -rf /home/ubuntu/devops-assessment/*
 
-sudo mv /home/ubuntu/devops-assessment-main/ /home/ubuntu/devops-assessment/
-
 # Move the contents of /home/ubuntu/devops-assessment-main/ to /home/ubuntu/devops-assessment/
 sudo mv /home/ubuntu/devops-assessment-main/* /home/ubuntu/devops-assessment/
 
