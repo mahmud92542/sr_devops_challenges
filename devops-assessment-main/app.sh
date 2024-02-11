9eb1b40768e86a45e98f986e4f1062e7dba79ef3
Mohammad Mahmud Hasan <34007876+mahmud92542@users.noreply.github.com>
2024-02-12 00:40:59 +0600

    Update app.sh

--- devops-assessment-main/app.sh
+++ devops-assessment-main/app.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 
+# Add echo commands to check script execution line by line
+echo "Running npm install..."
 npm install
+echo "npm install completed."
 
+echo "Installing pm2 globally..."
 npm install -g pm2
+echo "pm2 installation completed."
 
+echo "Installing Next.js..."
 npm install nextjs
+echo "Next.js installation completed."
 
 # Build the project
+echo "Building the project..."
 npm run build
+echo "Project build completed."
 
 # Start the "nextjs" process managed by PM2
+echo "Starting 'nextjs' process with PM2..."
 pm2 start npm --name "nextjs" -- start -- -p 3000
+echo "'nextjs' process started."
 
 # Reload NGINX
+echo "Reloading NGINX..."
 sudo systemctl reload nginx
+echo "NGINX reloaded."
