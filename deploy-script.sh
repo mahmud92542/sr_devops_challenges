a568c2a1b36c6869cb572bce2a9976ed07bfd38a
Mohammad Mahmud Hasan <34007876+mahmud92542@users.noreply.github.com>
2024-02-11 20:05:29 +0600

    Update deploy-script.sh

--- deploy-script.sh
+++ deploy-script.sh
@@ -4,7 +4,7 @@
 pm2 stop nextjs-app
 rm -rf /home/ubuntu/devops-assessment/*
 cp -R /home/ubuntu/devops-assessment/. /home/ubuntu/devops-assessment-main
-cd /home/ubuntu/devops-assessment-main
+cd /home/ubuntu/devops-assessment/devops-assessment-main
 npm run build
 pm2 start nextjs-app --name nextjs-app -- --port 3000
 sudo systemctl reload nginx
