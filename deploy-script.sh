3d711247f299f2ec4621e0f54a71417f7f0fae2f
Mohammad Mahmud Hasan <34007876+mahmud92542@users.noreply.github.com>
2024-02-11 21:46:49 +0600

    Update deploy-script.sh

--- deploy-script.sh
+++ deploy-script.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
-# Commands to execute
+# Stop the "nextjs" process managed by PM2
 pm2 stop nextjs
+
+# Remove all files and folders inside /home/ubuntu/devops-assessment/
 rm -rf /home/ubuntu/devops-assessment/*
-sudo mv /home/ubuntu/devops-assessment-main/ /home/ubuntu/devops-assessment/
+
+# Move the contents of /home/ubuntu/devops-assessment-main/ to /home/ubuntu/devops-assessment/
+sudo mv /home/ubuntu/devops-assessment-main/* /home/ubuntu/devops-assessment/
+
+# Change ownership of /home/ubuntu/devops-assessment/ to the "ubuntu" user and group
 sudo chown -R ubuntu:ubuntu /home/ubuntu/devops-assessment
+
+# Change directory to /home/ubuntu/devops-assessment/devops-assessment-main/
 cd /home/ubuntu/devops-assessment/devops-assessment-main/
-npm install nextjs
+
+# Install project dependencies
+npm install
+
+# Build the project
 npm run build
+
+# Start the "nextjs" process managed by PM2
 pm2 start npm --name "nextjs" -- start -- -p 3000
+
+# Reload NGINX
 sudo systemctl reload nginx
