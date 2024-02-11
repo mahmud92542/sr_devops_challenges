a1f7f5b416d4133662fdedc0ab70340938f9bdab
Mohammad Mahmud Hasan <34007876+mahmud92542@users.noreply.github.com>
2024-02-12 00:01:21 +0600

    Update deploy-script.sh

--- deploy-script.sh
+++ deploy-script.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-sudo chmod +x deploy-script.sh
-
 # Stop the "nextjs" process managed by PM2
 pm2 delete nextjs
 
@@ -10,14 +8,13 @@ rm -rf /home/ubuntu/devops-assessment/*
 
 # Move the contents of /home/ubuntu/devops-assessment-main/ to /home/ubuntu/devops-assessment/
 sudo mv /home/ubuntu/devops-assessment-main/ /home/ubuntu/devops-assessment/
-sudo mv /home/ubuntu/deploy-script.sh /home/ubuntu/devops-assessment/devops-assessment-main/
 
 # Change ownership of /home/ubuntu/devops-assessment/ to the "ubuntu" user and group
 sudo chown -R ubuntu:ubuntu /home/ubuntu/devops-assessment
 
 # Change directory to /home/ubuntu/devops-assessment/devops-assessment-main/
 cd /home/ubuntu/devops-assessment/devops-assessment-main/
-sudo chmod +x deploy-script.sh
+
 sudo npm install
 
 sudo npm install -g pm2
