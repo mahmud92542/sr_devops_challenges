51734c249010bb84171281099ebc44fdae8faa31
Mohammad Mahmud Hasan <34007876+mahmud92542@users.noreply.github.com>
2024-02-11 20:50:47 +0600

    Update deploy-script.sh

--- deploy-script.sh
+++ deploy-script.sh
@@ -3,7 +3,7 @@
 # Commands to execute
 pm2 stop nextjs-app
 rm -rf /home/ubuntu/devops-assessment/*
-sudo mv devops-assessment-main/ /home/ubuntu/devops-assessment/
+sudo mv /home/ubuntu/devops-assessment-main/ /home/ubuntu/devops-assessment/
 cd /home/ubuntu/devops-assessment/devops-assessment-main/
 npm install nextjs
 npm run build
